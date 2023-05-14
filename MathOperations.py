078ff649e9c12928ff87fb1f610b70fe9bca4623
MayoLayo <l.t05@outlook.com>
2023-05-14 21:01:19 +0200

    Math operations in Numpy

---
+++ MathOperations.py
@@ -0,0 +1,15 @@
+# Mathematical operations operate element wise.
+# Not on the array itself like regular python lists.
+
+import numpy as np
+a = np.array([1,2,3])
+
+# Adds 4 to all elements in the array.
+a = a + np.array([4])
+
+# Multiplies all elements in the array by 2.
+a = a * 2
+
+# Square roots the array.
+a = np.sqrt(a)
+
