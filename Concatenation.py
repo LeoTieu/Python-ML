712583dcb16ee28c7864e988faeb807ac2bc1a7f
MayoLayo <l.t05@outlook.com>
2023-05-15 20:03:15 +0200

    Vertical- and height stack

--- Concatenation.py
+++ Concatenation.py
@@ -14,3 +14,11 @@ print(c.shape) # (6,)
 
 c = np.concatenate((a,b.T), axis=1)
 print(c.shape) # (2, 3)
+
+# hstack and vstack
+a = np.array([1,2,3,4])
+b = np.array([5,6,7,8])
+v = np.vstack((a,b))
+h = np.hstack((a,b))
+print(v.shape) # (2, 4)
+print(h.shape) # (8,)
