918d08678b328226b02ad3e40a1986bac9e36e21
MayoLayo <l.t05@outlook.com>
2023-05-15 19:58:10 +0200

    Numpy Concatenation

---
+++ Concatenation.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+a = np.array([[1,2],[3,4]])
+b = np.array([[5,6]])
+
+# Concatenates a and b
+c = np.concatenate((a,b))
+print(c.shape) # (3, 2)
+
+# Default axis is 0
+# However, changing manually is possible.
+c = np.concatenate((a,b), axis=None)
+print(c.shape) # (6,)
+
+c = np.concatenate((a,b.T), axis=1)
+print(c.shape) # (2, 3)
